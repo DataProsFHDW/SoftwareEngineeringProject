a063b6c14b60d78e5e67e703a6dd423489436a6c
Balkon Chiller <m.wendorff09@gmail.com>
2023-03-04 15:37:07 +0100

    Added Reorder Function, ToDo as ContentCard

--- src/components/ToDoComponent.tsx
+++ src/components/ToDoComponent.tsx
@@ -1,14 +1,33 @@
-interface ToDoComponentProps {
-    id: string;
-    title: string;
-  }
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonReorder,
+  IonItem,
+} from "@ionic/react";
 
-export const ToDoComponent: React.FC<ToDoComponentProps> = ({id, title}) => {
-    return (
-        <div id={id}>
-            <p>{title}</p>
-        </div>
-    );
-  };
-  
-  
+interface ToDoComponentProps {
+  id: string;
+  title: string;
+}
+
+export const ToDoComponent: React.FC<ToDoComponentProps> = ({ id, title }) => {
+  return (
+    <IonItem>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>{title}</IonCardTitle>
+          <IonCardSubtitle>{id}</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Here's a small text description for the card content. Nothing more,
+          nothing less.
+        </IonCardContent>
+      </IonCard>
+      <IonReorder slot="end"></IonReorder>
+    </IonItem>
+  );
+};
--- src/pages/ToDoPage.tsx
+++ src/pages/ToDoPage.tsx
@@ -1,21 +1,21 @@
 import {
-  IonButton,
   IonButtons,
   IonContent,
   IonFab,
   IonFabButton,
   IonHeader,
   IonIcon,
+  IonList,
   IonMenuButton,
   IonPage,
+  IonReorderGroup,
   IonText,
   IonTitle,
   IonToolbar,
+  ItemReorderEventDetail,
 } from "@ionic/react";
 import { add } from "ionicons/icons";
-import { stringify } from "querystring";
 import { useState } from "react";
-import { useParams } from "react-router";
 import { ToDoComponent } from "../components/ToDoComponent";
 import "./Page.css";
 
@@ -23,15 +23,21 @@ export const ToDoPage: React.FC = () => {
   const [todoList, setTodoList] = useState<string[]>([]);
 
   let toDoRender = todoList.map((todo, index) => {
-    return(
-      <ToDoComponent
-        key={"ToDo-" + index}
-        id={"ToDo-" + index}
-        title={todo}
-      />
-    )
+    return (
+      <ToDoComponent key={"ToDo-" + index} id={"ToDo-" + index} title={todo} />
+    );
   });
-  let count = [1,2,3,4,5]
+
+  function handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log("Dragged from index", event.detail.from, "to", event.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    event.detail.complete();
+  }
 
   return (
     <IonPage>
@@ -49,11 +55,19 @@ export const ToDoPage: React.FC = () => {
           <IonText>Your trustworthy ToDo App</IonText>
           <br />
           <br />
-          {toDoRender}
+          <IonList>
+            {/* The reorder gesture is disabled by default, enable it to drag and drop items */}
+            <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
+              {toDoRender}
+            </IonReorderGroup>
+          </IonList>
           <IonFab slot="fixed" vertical="bottom" horizontal="end">
             <IonFabButton
               onClick={() => {
-                setTodoList([ ...todoList, "Test Todo" + todoList.length]);
+                setTodoList((todoList) => [
+                  ...todoList,
+                  "Test Todo" + todoList.length,
+                ]);
               }}
             >
               <IonIcon icon={add}></IonIcon>
