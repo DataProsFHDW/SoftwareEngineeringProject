b0adb7b35f34934472371009ea30567597249968
DDesktop <kada26@duck.com>
2023-03-17 13:48:54 +0100

    Rebased and implemented username

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ const App: React.FC = () => {
         setUsername(docUser.data()!.username); 
         if (!docUser.exists() || docUser.data()!.username == null) { console.log("Username Not set") }
       }
-    }
+    } 
     fetchUserProfile();
   }, [user]);
 
