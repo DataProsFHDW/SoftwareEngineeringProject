0d850195733e81b3dd3746f1376567065fbebb39
BalkonChiller <46871785+BalkonChiller@users.noreply.github.com>
2023-03-20 09:11:25 +0100

    Old Spielwiesen Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -38,12 +38,6 @@ const appPages: AppPage[] = [
     mdIcon: paperPlaneSharp
   },
   {
-    title: 'To-Dos 2',
-    url: '/todo2',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
     title: 'Favorites',
     url: '/page/Favorites',
     iosIcon: heartOutline,
