4d6fca996c18a1e0e5284164d9ceda552da15c90
BalkonChiller <46871785+BalkonChiller@users.noreply.github.com>
2023-04-13 12:31:58 +0200

    Display of Todo with Dates in Calendar

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -28,10 +28,87 @@ interface eventType {
   id: string;
   title: string;
   start: Date;
-  extendedProps: object;
+  extendedProps: any;
 }
 
-const events: eventType[] = [
+const events2: eventType[] = [
+  {
+      "id": "0niKODw68kCFcPfJ96xW",
+      "title": "Wild",
+      "start": new Date("2023-04-12T18:14:00.000Z"),
+      "extendedProps": {
+          "todoData": {
+              "id": "0niKODw68kCFcPfJ96xW",
+              "todoType": "Single ToDo",
+              "todoTitle": "Wild",
+              "todoDescription": "LOL collab",
+              "todoDate": "2023-04-12T18:14:00.000Z",
+              "users": [
+                  "cEtY5elpUJNOGC5TTHlkgjKkcrj1",
+                  "fZaZeIfJnZbkOfeaLNmfJkCPRcE2",
+                  "n1uK8YwZiNf8sM0ckjsea79puPw2"
+              ],
+              "isDeleted": false,
+              "isSynced": true
+          }
+      }
+  },
+  {
+      "id": "WLlcFWGPD2KuEnTEwoRb",
+      "title": "Grüsli de La",
+      "start": new Date("2023-04-15T20:25:00.000Z"),
+      "extendedProps": {
+          "todoData": {
+              "id": "WLlcFWGPD2KuEnTEwoRb",
+              "todoType": "Single ToDo",
+              "todoTitle": "Grüsli de La",
+              "todoDescription": "Simoms",
+              "todoDate": "2023-04-15T20:25:00.000Z",
+              "users": [
+                  "cEtY5elpUJNOGC5TTHlkgjKkcrj1",
+                  "n1uK8YwZiNf8sM0ckjsea79puPw2"
+              ],
+              "isDeleted": false,
+              "isSynced": true
+          }
+      }
+  },
+  {
+      "id": "g5cXYI0COJSnG7PHoTQP",
+      "title": "Hallo",
+      "start": new Date("2023-04-22T18:51:00.000Z"),
+      "extendedProps": {
+          "todoData": {
+              "id": "g5cXYI0COJSnG7PHoTQP",
+              "todoType": "Single ToDo",
+              "todoTitle": "Hallo",
+              "todoDescription": "Erde",
+              "todoDate": "2023-04-22T18:51:00.000Z",
+              "users": [
+                  "fZaZeIfJnZbkOfeaLNmfJkCPRcE2",
+                  "n1uK8YwZiNf8sM0ckjsea79puPw2"
+              ],
+              "isDeleted": false,
+              "isSynced": true
+          }
+      }
+  },
+  {
+    id: "1",
+    title: "Meeting",
+    start: new Date(),
+    extendedProps: {
+      todoData: {
+        todoType: TodoType.SIMPLE,
+        todoTitle: "Title",
+        todoDescription: "",
+        todoDate: new Date(),
+      },
+    },
+  },
+]
+/*
+[
   {
     id: "1",
     title: "Meeting",
@@ -46,30 +123,49 @@ const events: eventType[] = [
     },
   },
 ];
-
+*/
 export const CalendarPage: React.FC = () => {
   const todoStorage = useTodoStorage();
-  const todoList = todoStorage.getTodoList();
 
   const [weekendsVisible, setWeekendsVisible] = useState<boolean>(true);
-  const [currentEvents, setCurrentEvents] = useState<eventType[]>(events);
+  const [currentEvents, setCurrentEvents] = useState<eventType[] | undefined>();
   const [selectedTodo, setSelectedTodo] = useState<ITodoGroup>();
+  let currentEvents2: any[]| undefined = undefined
 
   const [presentEdit, dismissEdit] = useIonModal(TodoEditModal, {
     todoItem: selectedTodo,
     onDismiss: (data: ITodoGroup, role: string) => dismissEdit(data, role),
   });
+
   useEffect(() => {
     todoStorage.refreshTodos();
+    console.log(events2)
+    
   }, []);
 
+  useEffect(() => {
+    let timedTodoList = todoStorage.getTodoList().map((todo, index) => {
+      console.log("Date Value; " + todo.todoDate);
+      if (todo.todoDate) {
+        return {
+          "id": todo.id,
+          "title": todo.todoTitle.toString(),
+          "start": todo.todoDate,
+          "extendedProps": { "todoData": todo },
+        };
+      }
+    });
+    console.log(timedTodoList.filter( Boolean ));
+    setCurrentEvents(timedTodoList.filter( Boolean ) as eventType[]);
+  }, [todoStorage.storage]);
+
   const handleEditClick = (todo: ITodoGroup) => {
-    setSelectedTodo(todo)
+    setSelectedTodo(todo);
     presentEdit({
       onWillDismiss: (ev: CustomEvent<OverlayEventDetail>) => {
         if (ev.detail.role === "confirm") {
           console.log("Confirmed Input: " + JSON.stringify(ev.detail.data));
-          setSelectedTodo(ev.detail.data as ITodoGroup)
+          setSelectedTodo(ev.detail.data as ITodoGroup);
           console.log(selectedTodo);
         }
       },
@@ -107,11 +203,12 @@ export const CalendarPage: React.FC = () => {
           selectMirror={true}
           dayMaxEvents={true}
           weekends={weekendsVisible}
-          initialEvents={events} // alternatively, use the `events` setting to fetch from a feed
+          //initialEvents={events} // alternatively, use the `events` setting to fetch from a feed
+          events={currentEvents}
           select={handleDateSelect}
           eventContent={renderEventContent} // custom render function
           eventClick={handleEventClick}
-          eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+          // eventsSet={handleEvents} // called after events are initialized/added/changed/removed
 
           /* you can update a remote database when these fire:
             eventAdd={function(){}}
