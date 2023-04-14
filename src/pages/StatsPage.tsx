3799da25aed83a35a9a3ff9a037f10836b4c49c5
Tristan mag Marvin sehr gerne uwu <tristan.berthold2000@gmail.com>
2023-04-14 17:45:10 +0200

    Stats changes

--- src/pages/StatsPage.tsx
+++ src/pages/StatsPage.tsx
@@ -9,57 +9,36 @@ import { useTodoStorage } from '../storage/StateManagementWrapper';
 
 
 // Testen der Charts -> richtige Daten müssen noch eingepflegt werden
-const dataPieTest = [
-    {name: "Facebook", value:2000},
-    {name: "Instagram", value:1500},
-    {name: "Twitter", value:1000},
-    {name: "Telegram", value:500}, 
-]; 
-const dataBar = [
-    {
-      name: 'Page A',
+const dataArea = [
+  {
+      name: new Date(Date.now() - 6 * 24 * 60 * 60 * 1000).toLocaleDateString(),
       uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
+  },
+  {
+      name: new Date(Date.now() - 5 * 24 * 60 * 60 * 1000).toLocaleDateString(),
       uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
+  },
+  {
+      name: new Date(Date.now() - 4 * 24 * 60 * 60 * 1000).toLocaleDateString(),
       uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
+  },
+  {
+      name: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000).toLocaleDateString(),
       uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
+  },
+  {
+      name: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000).toLocaleDateString(),
       uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
+  },
+  {
+      name: new Date(Date.now() - 1 * 24 * 60 * 60 * 1000).toLocaleDateString(),
       uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
+  },
+  {
+      name: new Date().toLocaleDateString(),
       uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+  },
 ];
-
 
 type CardProps = {
   value1: string; 
@@ -92,18 +71,19 @@ const StatsPage: React.FC = () => {
   const { getTodoList } = useTodoStorage();
   const todos = getTodoList();
   const todoCount = todos.length;
-
   const SingleTodoCount = todos.filter(todo => todo.todoType.includes("Single")).length;
   const GroupTodoCount = todoCount - SingleTodoCount;
-
-  const TodosCreationDate = todos.filter(todo => todo.todoCreationDate).length;
-
-  //
+  const TodoOpenCount = todos.filter(todo => todo.isOpen === true).length;
+  const TodoClosedCount = todos.filter(todo => todo.isOpen === false).length;
+  const TodoOpenString = TodoOpenCount.toString()
+  const TodoClosedString = TodoClosedCount.toString()
   const value1A = 'Anzahl offene Todos';
-  const value2A = 'Zahl';
+  const value2A = TodoOpenString;
   const value1B = 'Anzahl geschlossene Todos';
-  const value2B = 'Zahl';
-  
+  const value2B = TodoClosedString;
+  const TodosDate = todos.filter(todo => todo.todoDate);
+  console.log(TodosDate);
+  console.log(todos);
 
   return (
     <IonPage>
@@ -116,68 +96,46 @@ const StatsPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <p className="grey">Here you find Statistics about your Todos.</p>
-        
+        <p className="grey">Here you find Statistics about your Todos.</p>      
           <div style={{ display: 'flex', justifyContent: 'center' }}>
               <Card value1={value1A} value2={value2A} />
               <Card value1={value1B} value2={value2B} />
-          </div>        
-        <ResponsiveContainer width={400} height={400}>
-          <PieChart width={400} height={400}>
-            <text x={200} y={20} textAnchor="middle" dominantBaseline="middle">
-              <tspan fontSize="18">Single vs. Group Todos</tspan>
-            </text>
-            <Pie
-              dataKey="value"
-              isAnimationActive={false}
-              data={[{ name: 'Single', value: SingleTodoCount }, { name: 'Group', value: GroupTodoCount }]}
-              //data = {dataPieTest}
-              cx="50%"
-              cy="50%"
-              outerRadius={80}
-              fill="#8884d8"
-              label
-            />
+          </div>
+          <AreaChart
+            width={window.innerWidth}
+            height={400}
+            data={dataArea}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
             <Tooltip />
-            <Legend verticalAlign="bottom" height={36} />
-          </PieChart>
-        </ResponsiveContainer>
-        <BarChart
-          width={500}
-          height={300}
-          data={dataBar}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
-        <AreaChart
-          width={500}
-          height={400}
-          data={dataBar}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
+            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+          </AreaChart>
+
+          <ResponsiveContainer width="100%" height={300}>
+            <PieChart>
+                <Pie
+                  dataKey="value"
+                  isAnimationActive={false}
+                  data={[{ name: 'Single', value: SingleTodoCount }, { name: 'Group', value: GroupTodoCount }]}
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#8884d8"
+                  label
+                />              
+                <Tooltip />
+                <Legend verticalAlign="bottom" height={36} />
+              </PieChart>
+            </ResponsiveContainer>
+
         <div> 
           <ul>
             <p>Anzahl: {todoCount}</p>
@@ -185,9 +143,23 @@ const StatsPage: React.FC = () => {
               <React.Fragment key={index}>               
                 <li>{todo.todoTitle}</li>
                 <li>{todo.id}</li>
+                
               </React.Fragment>
             ))}
           </ul>
+        </div>
+        <div>
+          {TodosDate.map((data, index) => (
+            <div key={index}>
+              {data ? (
+                <div>
+                  <div>Name: {data.todoDate ? data.todoDate.toLocaleString() : 'N/A'}</div>
+                </div>
+              ) : (
+                <p>No data available</p>
+              )}
+            </div>
+          ))}
         </div>  
       </IonContent>
     </IonPage>
